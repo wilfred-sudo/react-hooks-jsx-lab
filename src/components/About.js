0c74a84a8601918e4adcc00e1ef5080ecae23e59
wilfredkiprop <kipropwilfred90@gmail.com>
2025-04-12 22:13:19 +0300

    hooks jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; 
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I enjoy building user interfaces and solving problems with code.</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
